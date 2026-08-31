f9f3c63d56f98b034bb583e01637543c19fd257e
Nikita Ivanov <198op@mail.ru>
2026-08-31 21:32:46 +1000

    Add Yandex.Metrica counter

---
+++ README.md
@@ -0,0 +1,2 @@
+CV имени меня (на английском и русском)
+(https://nikitains.github.io/#about)
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat, Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
@@ -46,6 +47,31 @@ export default function RootLayout({
       >
         {children}
         <Toaster />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/112111027"
+              style={{ position: "absolute", left: -9999 }}
+              alt=""
+            />
+          </div>
+        </noscript>
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=112111027', 'ym');
+            ym(112111027, 'init', {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+            });
+          `}
+        </Script>
       </body>
     </html>
   );
